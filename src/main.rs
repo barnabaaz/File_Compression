09934b83fe32bdd74d116c54849f29cae82a172d
barnabaaz <barnabaaz@gmail.com>
2024-04-10 19:03:28 +0100

    Compressing files using GzEncoder

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use flate2::{write::GzEncoder, Compression};
+use std::env::args;
+use std::error::Error;
+use std::fs::File;
+use std::io::{copy, BufReader};
+use std::process;
+use std::time::Instant;
+struct Config {
+    filename: String,
+    compressed_filename: String,
+}
+impl Config {
+    fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let filename = match args.next() {
+            Some(args) => args,
+            None => return Err("didn't get source a file name"),
+        };
+        let compressed_filename = match args.next() {
+            Some(args) => format!("{}.gz", args),
+            None => return Err("didn't get target file name"),
+        };
+        Ok(Config {
+            filename,
+            compressed_filename,
+        })
+    }
+}
+fn main() {
+    let compression = Config::new(args()).unwrap_or_else(|err| {
+        eprintln!("[!] problem passing arguments {}", err);
+        process::exit(1)
+    });
+    if let Err(e) = run(compression) {
+        eprintln!("Application Error: {}", e);
+        process::exit(1)
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut input = BufReader::new(File::open(config.filename).unwrap_or_else(|err| {
+        eprintln!(
+            "[!] An error occured when trying to access the file : {}",
+            err
+        );
+        process::exit(1)
+    }));
+    let output = File::create(config.compressed_filename).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let time_taken = Instant::now();
+    copy(&mut input, &mut encoder).unwrap_or_else(|err| {
+        eprintln!("An error occured during compression: {}", err);
+        process::exit(1)
+    });
+    let output = encoder.finish().unwrap();
+    println!(
+        "Source Length {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+    println!("target Length {:?}", output.metadata().unwrap().len());
+    println!("Time Elasped for enconding {:?}", time_taken.elapsed());
+    Ok(())
+}
